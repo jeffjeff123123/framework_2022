8fe118e8c09ea8419d40e05cd0ec1eb45872ebc9
yuqiliu <liuyq1995@gmail.com>
2022-12-05 11:14:54 -0500

    test push

--- framework_JDBC_2022/src/ca/bytetube/demo/Demo.java
+++ framework_JDBC_2022/src/ca/bytetube/demo/Demo.java
@@ -3,5 +3,6 @@ package ca.bytetube.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test");
     }
 }
